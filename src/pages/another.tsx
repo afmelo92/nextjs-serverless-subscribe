39b40de545bc4a5b1baa656f55af3e3acb498039
Andre Melo <andre.fabian.melo@gmail.com>
2020-09-18 16:35:09 -0300

    Serveless NextJS subscribe system

--- src/pages/another.tsx
+++ src/pages/another.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // import { Container } from './styles';
 
 const Another: React.FC = () => {
-  return <h1>HELLOW FUCKING WORLD</h1>;
+  return <h1>ANOTHER PAGE</h1>;
 };
 
 export default Another;
